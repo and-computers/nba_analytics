096ea08a27263544cd1a1982d58cfd802b5fa91b
tosin <tosin.sonuyi@gmail.com>
2021-11-04 20:26:27 -0400

    add and rename data for 20/21 season

--- data/__init__.py
+++ data/__init__.py
@@ -1,9 +1,17 @@
 import os
 import pandas as pd
 
-SALARY_DATA = pd.read_csv(os.path.join(os.path.dirname(__file__), 'salary_contract_player_18.csv'))
-SALARY_DATA_21_22 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'salary_contract_player_21_22.csv'))
+SALARY_DATA_17_18 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'salary_contract_player_17_18.csv'))
+SALARY_DATA_20_21 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'salary_contract_player_20_21.csv'))
+
 # https://www.basketball-reference.com/leaders/per_active.html
-PER_DATA = pd.read_csv(os.path.join(os.path.dirname(__file__), 'PER_player_career_regszn_18.csv'))
-BPM_DATA = pd.read_csv(os.path.join(os.path.dirname(__file__), 'BPM_player_career_regszn_18.csv'))
-TM_DATA = pd.read_csv(os.path.join(os.path.dirname(__file__), 'team_data_18.csv'))
+# https://www.basketball-reference.com/leaders/per_career.html
+PER_DATA_17_18 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'PER_player_career_regszn_17_18.csv'))
+PER_DATA_20_21 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'PER_player_career_regszn_20_21.csv'))
+
+
+BPM_DATA_17_18 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'BPM_player_career_regszn_17_18.csv'))
+
+
+TM_DATA_17_18 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'team_data_17_18.csv'))
+TM_DATA_20_21 = pd.read_csv(os.path.join(os.path.dirname(__file__), 'team_data_20_21.csv'))
